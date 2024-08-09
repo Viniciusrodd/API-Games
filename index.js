7014c63a02a46e459ce04ebdb06bb95ffb5260fd
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-08-09 11:24:51 -0300

    Criando um protótipo de banco de dados falso com JSON

--- index.js
+++ index.js
@@ -9,3 +9,35 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+
+//BANCO DE DADOS FALSE:
+var BD = {
+    games:[
+        {
+            id: 23,
+            title: 'Call of Duty MW',
+            year: 2019,
+            price: 60
+        },
+        {
+            id: 65,
+            title: 'Sea of Thieves',
+            year: 2018,
+            price: 40
+        },
+        {
+            id: 2,
+            title: 'Minecraft',
+            year: 2012,
+            price: 20
+        }
+    ]
+}
+
+
+
+
+//ABRINDO SERVER:
+app.listen(4545, () =>{
+    console.log('Server created');
+})
