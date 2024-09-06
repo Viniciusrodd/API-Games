d1a818f7dbfc40e710a73f4bdcef38d742246a81
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-09-06 15:30:00 -0300

    Inserindo token no localStorage e chamando ela no cabeçalho de autorização do axios

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,12 +8,6 @@ function Auth(req, res, next){
 
     if(authToken != undefined){
         //separando a parte de 'Bearer' do 'tokien' em si: 
-        /*  
-            Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
-            eyJpZCI6MSwiZW1haWwiOiJ2aW5pY2l1c3VzZXIyMkBnb
-            WFpbC5jb20iLCJpYXQiOjE3MjU0NjA5NTQsImV4cCI6MTcyNTYzMzc1NH0.
-            S-OfXwvpCI_rfb3rbuQGyVEBxr0E8kak02PJgumiUOs'
-        */
         const bearer = authToken.split(' ');
         var token = bearer[1];
 
